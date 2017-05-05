2804aeebbb2ea2fa52e2efeb3b9bb47b0aa35283
NoahPeeters <noah.peeters@icloud.com>
2017-05-05 13:14:19 +0200

    Initial Commit

---
+++ TCPTester/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TCPTester
+//
+//  Created by Noah Peeters on 5/5/17.
+//  Copyright © 2017 Noah Peeters. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
